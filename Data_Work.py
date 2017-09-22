6db30c50a31b4d14f7698008eb5b2e2c431aed6c
Kshitij <kgrovor@gmail.com>
2017-09-22 21:28:17 +0530

    Stemming and optimisations with py file

--- Data_Work.py
+++ Data_Work.py
@@ -1,15 +1,35 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[5]:
 
 import nltk
 from nltk import word_tokenize
+from nltk.corpus import stopwords
 import numpy as np
+from nltk.stem import PorterStemmer
 #nltk.download()
 
 
-# In[3]:
+# In[11]:
+
+data = ["The iPhone 8 and 8 Plus, two of the three new iPhones in Apple’s 2017 lineup, went on sale around the world, starting in Australia where hundreds of people have usually gathered outside Apple’s Sydney city store for previous launches. But instead of queues winding down the street there were fewer than 30 people lining up before the store opened on Friday."," At first, East Central University — a public university with 4,000 students in Ada, about 80 miles (130 kilometers) southeast of Oklahoma City — complied with the request from Americans United for Separation of Church and State, removing Bibles and other Christian-themed items from the colonial-style chapel that was donated by a longtime regent in 1957. But before the cross could be taken down, the matter had drawn the attention of religious leaders as well as Oklahoma's Republican attorney general, who is running for re-election next year."]
+
+
+def basic(data):
+    proc_data = []
+    stop_words = set(stopwords.words('english'))
+    stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])
+    for doc in data:
+        proc_data.append([i.lower() for i in (PorterStemmer().stem(x) for x in word_tokenize(doc)) if i.lower() not in stop_words])
+    return proc_data
+
+    
+data = basic(data)
+print(data)
+
+
+# In[12]:
 
 class defaultlist(list):
 
@@ -22,14 +42,14 @@ class defaultlist(list):
     
 
 
-# In[85]:
+# In[13]:
 
-data = ["We are implementing tf idf"," the the the the implementing"]
+#data = ["We are implementing tf idf"," the the the the implementing"]
         
         
 tdm = {}
 for docid in range(len(data)):
-    for i in word_tokenize(data[docid]):
+    for i in data[docid]:
         
         if i not in tdm:
             tdm[i]= defaultlist([0])
@@ -44,26 +64,16 @@ for docid in range(len(data)):
 print(tdm)
 
 
-# In[86]:
-
-max_len = 0
-ln = 0
-for ech in tdm:
-    ln = len(tdm[ech])
-    #print(tdm[ech], " ", len(tdm[ech]))
-    if(ln >= max_len):
-        max_len = ln
-#print(max_len) 
+# In[16]:
 
 for ech in tdm:
-    if len(tdm[ech]) < max_len:
-        tdm[ech][max_len-1]=0
-        
+    if len(tdm[ech]) < len(data):
+        tdm[ech][len(data)-1]=0
         
 print(tdm)
 
 
-# In[87]:
+# In[18]:
 
 from math import *
 docs_vect = []
@@ -73,17 +83,11 @@ for docid in range(len(data)):
     vectmap.clear()
     sq = 0
     for a in vals:
-        #print(a)
         sq = sq + (a[docid] * a[docid])
-        #print(a[docid], " a ")
-    #print(sq, "aa")
     sq = sqrt(sq)
-    #print(sq , " |")
-    for word in word_tokenize(data[docid]):
+    for word in data[docid]:
         tf = tdm[word][docid]/sq
-        #print(tf)
         idf = log(len(data)/np.count_nonzero(tdm[word]),10)
-        #print("idf = ", idf)
         tfidf = tf * idf
         vectmap[word] = tfidf
     docs_vect.append(vectmap)
@@ -92,8 +96,7 @@ print(docs_vect)
         
 
 
-# In[58]:
+# In[ ]:
 
-vals = list(tdm.values())
-print(vals)
+
 
